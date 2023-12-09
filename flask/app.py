3efaf95898f29a9ec89e5439070d3b58cb91ee39
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2023-12-09 06:03:30 +0000

    mensaje

--- flask/app.py
+++ flask/app.py
@@ -1,26 +1,37 @@
 from flask import Flask, request, jsonify
 import pandas as pd
-import numpy as np  # Importa NumPy
+import numpy as np
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, origins='*')
 
-# Cargar datos desde el archivo CSV
-data = pd.read_csv('data.csv')
+# Obtener el número total de filas en el archivo CSV
+total_filas = sum(1 for _ in open('data.csv'))
+
+# Calcular el valor de skiprows para leer los últimos 10 millones de datos
+skip_rows = max(0, total_filas - 20000000)
+
+# Cargar los últimos 10 millones de datos desde el archivo CSV
+data_parte_otra_instancia = pd.read_csv('data.csv', skiprows=range(1, skip_rows + 1), nrows=10000000)
+segunda_fila = data_parte_otra_instancia.iloc[1:2]
+print("Segunda fila:\n", segunda_fila)
+# Imprimir la última fila
+ultima_fila = data_parte_otra_instancia.iloc[-1:]
+print("\nÚltima fila:\n", ultima_fila)
 
 @app.route('/procesar', methods=['POST'])
 def procesar():
     try:
         user_data = request.json
-        print('Usuario recibido:', user_data)
+        print('Usuario recibido en la otra instancia:', user_data)
 
         # Eliminar la clave '_id' del usuario recibido
         user_data.pop('_id', None)
 
         # Convertir datos a arrays NumPy para cálculos más eficientes
         user_ratings = np.array([int(user_data[f'pelicula{i}']) for i in range(1, 6)])
-        data_ratings = data.iloc[:, 1:6].values.astype(int)
+        data_ratings = data_parte_otra_instancia.iloc[:, 1:6].values.astype(int)
 
         # Calcular la distancia euclidiana entre el usuario enviado y todos los usuarios en el conjunto de datos
         distancias = np.linalg.norm(data_ratings - user_ratings, axis=1)
@@ -28,36 +39,23 @@ def procesar():
         # Encontrar el índice del usuario con la distancia euclidiana más baja
         vecino_mas_cercano_index = np.argmin(distancias)
 
-        vecino_mas_cercano = data.iloc[vecino_mas_cercano_index].to_dict()
+        vecino_mas_cercano_otra_instancia = data_parte_otra_instancia.iloc[vecino_mas_cercano_index].to_dict()
 
-        # Obtener la película recomendada del vecino más cercano
-        pelicula_recomendada = obtener_pelicula_recomendada(vecino_mas_cercano)
+        # Incluir la distancia en la respuesta
+        distancia_del_vecino_mas_cercano = distancias[vecino_mas_cercano_index]
 
         # Preparar la respuesta
         respuesta = {
-            'vecino_mas_cercano': vecino_mas_cercano,
-            'pelicula_recomendada': pelicula_recomendada,
-	    'usuario_recibido': user_data['usuario'],
+            'vecino_mas_cercano': vecino_mas_cercano_otra_instancia,
+            'distancia_del_vecino_mas_cercano': distancia_del_vecino_mas_cercano,
+            'usuario_recibido': user_data['usuario'],
         }
 
         return jsonify(respuesta)
-        
+
     except Exception as e:
-        print('Error en la función procesar:', str(e))
-        return jsonify({'error': 'Error interno'}), 500
-
-def obtener_pelicula_recomendada(vecino):
-    # Lógica para obtener la película recomendada del vecino más cercano
-    # En este caso, se elige la película con la calificación más alta entre las películas 6 a 10
-
-    # Crear una lista de tuplas (pelicula, calificacion) para las películas 6 a 10
-    peliculas_calificaciones = [(f'pelicula{i}', vecino[f'pelicula{i}']) for i in range(6, 11)]
-
-    # Encontrar la película con la calificación más alta
-    pelicula_recomendada, _ = max(peliculas_calificaciones, key=lambda x: x[1])
-
-    return pelicula_recomendada
+        print('Error en la función procesar en la otra instancia:', str(e))
+        return jsonify({'error': 'Error interno en la otra instancia'}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
+    app.run(host='0.0.0.0', port=5000, debug=True)  # Puerto diferente para la otra instancia
--- flask/seeder.py
+++ flask/seeder.py
@@ -2,7 +2,7 @@ import csv
 import random
 
 # Número de usuarios
-num_users = 100000
+num_users = 50000000
 
 # Generar datos de ejemplo y escribir al archivo CSV
 with open('data.csv', 'w', newline='') as csvfile:
@@ -16,16 +16,16 @@ with open('data.csv', 'w', newline='') as csvfile:
     for user_id in range(1, num_users + 1):
         row_data = {
             'usuario': f'User{user_id}',
-            'pelicula1': random.randint(1, 10),
-            'pelicula2': random.randint(1, 10),
-            'pelicula3': random.randint(1, 10),
-            'pelicula4': random.randint(1, 10),
-            'pelicula5': random.randint(1, 10),
-            'pelicula6': random.randint(1, 10),
-            'pelicula7': random.randint(1, 10),
-            'pelicula8': random.randint(1, 10),
-            'pelicula9': random.randint(1, 10),
-            'pelicula10': random.randint(1, 10)
+            'pelicula1': random.randint(1, 100),
+            'pelicula2': random.randint(1, 100),
+            'pelicula3': random.randint(1, 100),
+            'pelicula4': random.randint(1, 100),
+            'pelicula5': random.randint(1, 100),
+            'pelicula6': random.randint(1, 100),
+            'pelicula7': random.randint(1, 100),
+            'pelicula8': random.randint(1, 100),
+            'pelicula9': random.randint(1, 100),
+            'pelicula10': random.randint(1, 100)
         }
         writer.writerow(row_data)
 
